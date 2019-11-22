362508e88b7a13877d8a96569975bad8f5941f7f
Lucien Cartier-Tilet <lucien@phundrak.com>
2019-11-22 12:02:50 +0100

    initial commit, everything seems to work fine

---
+++ README.md
@@ -0,0 +1,70 @@
+- [About](#sec-1)
+- [Dependencies](#sec-2)
+- [Invite TranslAAAAAAAAAAAAAAAAAtor](#sec-3)
+- [Running the project](#sec-4)
+- [Licence](#sec-5)
+- [Contributing](#sec-6)
+
+![img](https://cdn.rawgit.com/syl20bnr/spacemacs/442d025779da2f62fc86c2082703697714db6514/assets/spacemacs-badge.svg)
+
+# About<a id="sec-1"></a>
+
+TranslAAAAAAAAAAAAAAAAAtor is a meme bot inspired by the subreddit [r/AAAAAAAAAAAAAAAAA](https://www.reddit.com/r/AAAAAAAAAAAAAAAAA/). What it does is translate a sentence given to it by replacing all characters (except whitespace) into As. That’s it. That’s literally it.
+
+# Dependencies<a id="sec-2"></a>
+
+The only dependency of translAAAAAAAAAAAAAAAAAtor is [Discord.py](https://github.com/Rapptz/discord.py) which powers the bot itself.
+
+# Invite TranslAAAAAAAAAAAAAAAAAtor<a id="sec-3"></a>
+
+Maybe you do not want to build the bot itself, but instead you just want to invite it on your server? I got you covered! You can invite the official instance of TranslAAAAAAAAAAAAAAAAAtor by clicking [here](https://discordapp.com/api/oauth2/authorize?client_id=603207431157317632&permissions=67584&scope=bot)!
+
+Be aware though that I cannot ensure 24/7 uptime of TranslAAAAAAAAAAAAAAAAAtor. If you notice it is down, shoot me a tweet (faster), a DM (fast, P'undrak#0001) or an email (slower).
+
+# Running the project<a id="sec-4"></a>
+
+First, you will need docker and docker-compose installed on your machine. If they aren’t installed yet, go check Docker’s or your distro’s instruction on how to get them.
+
+To Run This Project, you will only need the [docker-compose.yml](docker-compose.yml) file from this repository. You can get it by running the following command on your machine:
+
+```sh
+wget -c https://raw.githubusercontent.com/phundrak/translaaaaaaaaaaaaaaaaator/master/docker-compose.yml
+```
+
+Or you can clone the whole project if you wish.
+
+Due to obvious security reasons, I am not giving away the token of my bot in the source code, and you should not either. This is why this project will try to get it from your environment variables. To do so, edit the docker-compose file to add the environment key \`BOT<sub>TOKEN</sub>\`, like so:
+
+```yaml
+version: '3'
+services:
+  bot:
+    image: phundrak/translaaaaaaaaaaaaaaaaator:latest
+    command: python translaaaaaaaaaaaaaaaaator.py
+    environment:
+      - BOT_TOKEN=yourtoken
+    volumes:
+      - .:/code
+```
+
+You can now run the project within a contained environment by running the following command:
+
+```sh
+docker-compose up
+```
+
+If you wish to have it detached from your terminal, you can add the \`-d\` switch to the command:
+
+```sh
+docker-compose up -d
+```
+
+And that’s it! Your bot should be up and running once your container is built, which happens only the first time you launch it. If you want to stop TranslAAAAAAAAAAAAAAAAAtor, simply hit `Ctrl-c` in your terminal if you haven’t detached the process, or run `docker-compose down` if you did.
+
+# Licence<a id="sec-5"></a>
+
+This project is under the free GPLv3 licence. You can find the complete licence in <LICENCE.md>.
+
+# Contributing<a id="sec-6"></a>
+
+Submit a pull request, and I’ll review it when I’ll have time to (this is not a project I will pour a lot of efforts into). Simply be aware that I’d prefer it if your code is written with meaningful variables (I should fix that), comments (I should fix that), and be aware that readability is above all. Also, don’t be an asshole (that’s the project’s Code of Conduct for y’all).
---
+++ translaaaaaaaaaaaaaaaaator.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# TranslAAAAAAAAAAAAAAAAAtor: sauce metadata finder from nhentai.net for Discord
+# Copyright (C) 2019 Lucien "Phundrak" Cartier-Tilet
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+"""TranslAAAAAAAAAAAAAAAAAtorBot.
+
+Discord bot for nhentai sauce.
+
+Usage:
+  saucebot <token>
+
+Options:
+  <token>    Bot's Discord API token
+  -h --help  Show this screen"""
+
+import asyncio
+import re
+import os
+
+import discord
+from discord.ext import commands
+
+BOT_PREFIX = 'aa!'
+BOT_LINK = "https://labs.phundrak.fr/phundrak/TranslAAAAAAAAAAAAAAAAAtor",
+
+
+class TranslAAAAAAAAAAAAAAAAAtorBot:
+    def __init__(self):
+        """Initialize the bot using the Discord token.
+
+        :param bot_token: Full secret bot token
+        """
+        self.token = os.getenv('BOT_TOKEN')
+        self.bot = commands.Bot(command_prefix=BOT_PREFIX, help_command=None)
+        self.setup()
+
+    def setup(self):
+        @self.bot.event
+        @asyncio.coroutine
+        async def on_ready():
+            print("Bot online!")
+            print("Name: {}".format(self.bot.user.name))
+            print("ID: {}".format(self.bot.user.id))
+            await self.bot.change_presence(activity=discord.Game(
+                name="aa!help"))
+
+        @self.bot.event
+        @asyncio.coroutine
+        async def on_message(message):
+            # ignore self messages
+            if message.author.bot:
+                return
+            if message.content is None:
+                print("Empty message received.")
+                return
+            if message.content.startswith(BOT_PREFIX):
+                if message.content.startswith(BOT_PREFIX + "a"):
+                    text = message.content[5:]
+                    print(text)
+                    await message.channel.send(re.sub(r"[^\s]", "A", text))
+                else:
+                    await self.bot.process_commands(message)
+            # else:
+            #     await message.channel.trigger_typing()
+            #     # await message.channel.send(embed=embed)
+
+        @self.bot.command()
+        @asyncio.coroutine
+        async def help(ctx):
+            embed = discord.Embed(
+                title="TranslAAAAAAAAAAAAAAAAAtor help",
+                description="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                color=0xeee657)
+            embed.add_field(name="`aa!a bbbbb`",
+                            value="AAAAA",
+                            inline=False)
+            embed.add_field(name="AAAAAA AAAA", value=BOT_LINK, inline=False)
+            await ctx.send(embed=embed)
+
+        # @self.bot.command()
+        # @asyncio.coroutine
+        # async def a(ctx, text):
+        #     await ctx.channel.trigger_typing()
+        #     print(text)
+        #     await ctx.send(re.sub(r"[^\s]", "A", text))
+        #     # embed = self.translate(text)
+        #     # await ctx.send(embed=embed)
+
+    def run(self):
+        self.bot.run(self.token)
+
+    def translate(text):
+        return
+
+
+bot = TranslAAAAAAAAAAAAAAAAAtorBot()
+bot.run()
